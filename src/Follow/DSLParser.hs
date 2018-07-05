d024e7c9df0ffcf6054141062d5080ac9d175d1b
Marc Busqué <marc@lamarciana.com>
2018-07-05 18:02:55 +0200

    Add version and description to the DSL

--- src/Follow/DSLParser.hs
+++ src/Follow/DSLParser.hs
@@ -7,15 +7,33 @@ module Follow.DSLParser
 import Text.Parsec
 
 data ParsedDSL = ParsedDSL
-  { parsedDSLTitle :: String
-  }
+  {  parsedDSLVersion :: String
+  ,  parsedDSLTitle :: String
+  ,  parsedDSLDescription :: String
+  } deriving (Show)
 
 type ParsedResult = Either ParseError ParsedDSL
 
-format :: Parsec String () String
-format = spaces *> string "FOLLOW" *> spaces *> many1 anyChar
+format :: Parsec String () (String, String, String)
+format = do
+  version <- versionFormat
+  title <- titleFormat
+  description <- descriptionFormat
+  return (version, title, description)
+
+versionFormat :: Parsec String () String
+versionFormat = spaces *> string "VERSION" *> spaces *> many1 (noneOf "\n\r") <* endOfLine
+
+titleFormat :: Parsec String () String
+titleFormat = spaces *> string "FOLLOW" *> spaces *> many1 (noneOf "\n\r") <* endOfLine
+
+descriptionFormat :: Parsec String () String
+descriptionFormat = spaces *> string "DESCRIBED BY" *> spaces *> many1 (noneOf "\n\r") <* optional endOfLine
 
 parseDSL :: String -> ParsedResult
 parseDSL toParse = case parse format "(source)" toParse of
   Left error -> Left error
-  Right title -> Right (ParsedDSL { parsedDSLTitle = title })
+  Right (version, title, description) -> Right (ParsedDSL {
+                                                     parsedDSLVersion = version
+                                                   , parsedDSLTitle = title
+                                                   , parsedDSLDescription = description })
--- test/Follow/DSLParserSpec.hs
+++ test/Follow/DSLParserSpec.hs
@@ -6,7 +6,20 @@ import Follow.DSLParser
 
 spec :: Spec
 spec = do
-  describe ".parse" $ do
+  describe ".parseDSL" $ do
+    let success = unlines [
+            "VERSION 1.0"
+          , "FOLLOW foo"
+          , "DESCRIBED BY description"]
+    
+    it "copies VERSION value as value" $ do
+      let (Right parsed) = parseDSL success
+      parsedDSLVersion parsed `shouldBe` "1.0"
+
     it "copies FOLLOW value as title" $ do
-      let (Right parsed) = parseDSL("FOLLOW foo")
+      let (Right parsed) = parseDSL success
       parsedDSLTitle parsed `shouldBe` "foo"
+
+    it "copies DESCRIBED BY value as description" $ do
+      let (Right parsed) = parseDSL success
+      parsedDSLDescription parsed `shouldBe` "description"
