8c85184c2d67e9314719f8f8f4bffa97a42e8f36
Marc Busqué <marc@lamarciana.com>
2018-07-07 08:35:35 +0200

    Format code with hindent

--- src/Follow/DSLParser.hs
+++ src/Follow/DSLParser.hs
@@ -13,23 +13,24 @@ That recipe could be parsed with 'parseDSL', which would return a
 'ParsedDSL' data type with the extracted information.
 -}
 module Follow.DSLParser
-  (
-    parseDSL
-  , ParsedDSL (..)
+  ( parseDSL
+  , ParsedDSL(..)
   , ParsedResult
   ) where
 
 import           Text.Parsec
 
 type PVersion = String
+
 type PTitle = String
+
 type PDescription = String
 
 -- | Haskell representation of the information extracted from the DSL.
 data ParsedDSL = ParsedDSL
-  {  pVersion     :: PVersion -- ^ Version of the DSL used.
-  ,  pTitle       :: PTitle -- ^ Title for the recipe; what is being followed.
-  ,  pDescription :: PDescription -- ^ A description for the recipe
+  { pVersion     :: PVersion -- ^ Version of the DSL used.
+  , pTitle       :: PTitle -- ^ Title for the recipe; what is being followed.
+  , pDescription :: PDescription -- ^ A description for the recipe
   } deriving (Show)
 
 -- | The result of a parsing: a 'ParsedDSL' or an error.
@@ -43,16 +44,22 @@ format = do
   return (version, title, description)
 
 versionFormat :: Parsec String () PVersion
-versionFormat = spaces *> string "VERSION" *> spaces *> many1 (noneOf "\n\r") <* endOfLine
+versionFormat =
+  spaces *> string "VERSION" *> spaces *> many1 (noneOf "\n\r") <* endOfLine
 
 titleFormat :: Parsec String () PTitle
-titleFormat = spaces *> string "FOLLOW" *> spaces *> many1 (noneOf "\n\r") <* endOfLine
+titleFormat =
+  spaces *> string "FOLLOW" *> spaces *> many1 (noneOf "\n\r") <* endOfLine
 
 descriptionFormat :: Parsec String () PDescription
-descriptionFormat = spaces *> string "DESCRIBED BY" *> spaces *> many1 (noneOf "\n\r") <* optional endOfLine
+descriptionFormat =
+  spaces *> string "DESCRIBED BY" *> spaces *> many1 (noneOf "\n\r") <*
+  optional endOfLine
 
 -- | Parses given DSL to a 'ParsedDSL' on success.
 parseDSL :: String -> ParsedResult
-parseDSL toParse = case parse format "(source)" toParse of
-  Left error -> Left error
-  Right (version, title, description) -> Right (ParsedDSL version title description)
+parseDSL toParse =
+  case parse format "(source)" toParse of
+    Left error -> Left error
+    Right (version, title, description) ->
+      Right (ParsedDSL version title description)
