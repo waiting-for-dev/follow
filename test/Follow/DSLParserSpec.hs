22edae2c0eca1deb8cabb52496db33455182e67f
Marc Busqué <marc@lamarciana.com>
2018-07-07 08:38:56 +0200

    Format code in test file

--- test/Follow/DSLParserSpec.hs
+++ test/Follow/DSLParserSpec.hs
@@ -1,25 +1,20 @@
 module Follow.DSLParserSpec where
 
-import Test.Hspec
+import           Test.Hspec
 
-import Follow.DSLParser
+import           Follow.DSLParser
 
 spec :: Spec
 spec = do
   describe ".parseDSL" $ do
-    let success = unlines [
-            "VERSION 1.0"
-          , "FOLLOW foo"
-          , "DESCRIBED BY description"]
-    
+    let success =
+          unlines ["VERSION 1.0", "FOLLOW foo", "DESCRIBED BY description"]
     it "copies VERSION value as value" $ do
       let (Right parsed) = parseDSL success
       pVersion parsed `shouldBe` "1.0"
-
     it "copies FOLLOW value as title" $ do
       let (Right parsed) = parseDSL success
       pTitle parsed `shouldBe` "foo"
-
     it "copies DESCRIBED BY value as description" $ do
       let (Right parsed) = parseDSL success
       pDescription parsed `shouldBe` "description"
