10aa7cf13b95374b0d0641d7e82959fb6cc0808d
Marc Busqué <marc@lamarciana.com>
2018-07-20 15:51:46 +0200

    Remove Null strategy

--- src/Follow/Strategies/Null.hs
+++
@@ -1,8 +0,0 @@
-{- | Description: Null strategies which does nothing
--}
-module Follow.Strategies.Null where
-
-import           Follow.Strategies (ArgumentsDSL)
-
-argumentsDSL :: ArgumentsDSL
-argumentsDSL = []
