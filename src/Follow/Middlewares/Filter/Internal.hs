943a45f2a51904df18c5c2c5c0110102b7fdb03b
Marc Busqué <marc@lamarciana.com>
2018-08-21 16:58:02 +0200

    Add more predicates in filter middleware

--- src/Follow/Middlewares/Filter/Internal.hs
+++ src/Follow/Middlewares/Filter/Internal.hs
@@ -1,10 +1,28 @@
 module Follow.Middlewares.Filter.Internal
   ( equalP
+  , containP
+  , prefixP
+  , suffixP
   ) where
 
+import           Data.Maybe   (fromMaybe)
+import           Data.Text    (Text)
+import qualified Data.Text    as T (isInfixOf, isPrefixOf, isSuffixOf)
 import           Follow.Types (Entry, MFilterPredicate)
 
 type EntryFieldGetter a = Entry -> Maybe a
 
 equalP :: Eq a => EntryFieldGetter a -> a -> MFilterPredicate
-equalP getter x entry = getter entry == Just x
+equalP = liftP (==)
+
+containP :: EntryFieldGetter Text -> Text -> MFilterPredicate
+containP = liftP T.isInfixOf
+
+prefixP :: EntryFieldGetter Text -> Text -> MFilterPredicate
+prefixP = liftP T.isPrefixOf
+
+suffixP :: EntryFieldGetter Text -> Text -> MFilterPredicate
+suffixP = liftP T.isSuffixOf
+
+liftP :: (b -> a -> Bool) -> EntryFieldGetter a -> b -> MFilterPredicate
+liftP p getter x entry = maybe False (p x) (getter entry)
--- test/Follow/Middlewares/Filter/InternalSpec.hs
+++ test/Follow/Middlewares/Filter/InternalSpec.hs
@@ -7,7 +7,7 @@ import           Follow.Types                       (Entry (..))
 import           Test.Hspec
 
 spec :: Spec
-spec =
+spec = do
   describe ".equalP" $ do
     it "returns true on equality" $ do
       let entry = Entry (Just "http://url.com") Nothing Nothing Nothing Nothing
@@ -18,3 +18,39 @@ spec =
     it "returns false when field is Nothing" $ do
       let entry = Entry Nothing Nothing Nothing Nothing Nothing
       (eTitle `equalP` "Title") entry `shouldBe` False
+  describe ".containP" $ do
+    it "returns true when needle is contained" $ do
+      let entry =
+            Entry
+              (Just "http://this_is_a_url.com")
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+      (eURI `containP` "url") entry `shouldBe` True
+    it "returns false when needle is not contained" $ do
+      let entry = Entry Nothing (Just "GUID1") Nothing Nothing Nothing
+      (eGUID `containP` "NO") entry `shouldBe` False
+    it "returns false when field is Nothing" $ do
+      let entry = Entry Nothing Nothing Nothing Nothing Nothing
+      (eTitle `containP` "Title") entry `shouldBe` False
+  describe ".prefixP" $ do
+    it "returns true when needle is prefix" $ do
+      let entry = Entry (Just "http://url.com") Nothing Nothing Nothing Nothing
+      (eURI `prefixP` "http://") entry `shouldBe` True
+    it "returns false when needle is not prefix" $ do
+      let entry = Entry Nothing (Just "GUID1") Nothing Nothing Nothing
+      (eGUID `prefixP` "NO") entry `shouldBe` False
+    it "returns false when field is Nothing" $ do
+      let entry = Entry Nothing Nothing Nothing Nothing Nothing
+      (eTitle `prefixP` "Title") entry `shouldBe` False
+  describe ".suffixP" $ do
+    it "returns true when needle is suffix" $ do
+      let entry = Entry (Just "http://url.com") Nothing Nothing Nothing Nothing
+      (eURI `suffixP` ".com") entry `shouldBe` True
+    it "returns false when needle is not suffix" $ do
+      let entry = Entry Nothing (Just "GUID1") Nothing Nothing Nothing
+      (eGUID `suffixP` "NO") entry `shouldBe` False
+    it "returns false when field is Nothing" $ do
+      let entry = Entry Nothing Nothing Nothing Nothing Nothing
+      (eTitle `suffixP` "Title") entry `shouldBe` False
